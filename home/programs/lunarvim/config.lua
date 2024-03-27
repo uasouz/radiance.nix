c301ae8e23439c16812b60d016c28582944aac8f
uasouz <vlopes45@gmail.com>
2024-03-26 22:42:25 -0400

    add minimap shortcut

--- home/programs/lunarvim/config.lua
+++ home/programs/lunarvim/config.lua
@@ -108,8 +108,11 @@ lvim.plugins = {
   {
     'wfxr/minimap.vim',
     build = "cargo install --locked code-minimap",
-    --cmd = { "Minimap", "MinimapClose", "MinimapToggle", "MinimapRefresh", "MinimapUpdateHighlight" },
+    -- cmd = { "Minimap", "MinimapClose", "MinimapToggle", "MinimapRefresh", "MinimapUpdateHighlight" },
     config = function()
+      lvim.builtin.which_key.mappings["m"] = {
+        "<cmd>Minimap<CR>", "Minimap"
+      }
       vim.cmd("let g:minimap_width = 10")
       vim.cmd("let g:minimap_auto_start = 1")
       vim.cmd("let g:minimap_auto_start_win_enter = 1")
