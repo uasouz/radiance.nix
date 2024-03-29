ed5176f87646a80ffb7cb84749c80195f088228a
uasouz <vlopes45@gmail.com>
2024-03-28 22:41:24 -0400

    make lvim transparent

--- home/programs/lunarvim/config.lua
+++ home/programs/lunarvim/config.lua
@@ -15,6 +15,9 @@ lvim.format_on_save = {
   pattern = "*.lua",
   timeout = 1000,
 }
+
+lvim.transparent_window = true;
+
 -- to disable icons and use a minimalist setup, uncomment the following
 -- lvim.use_icons = false
 
